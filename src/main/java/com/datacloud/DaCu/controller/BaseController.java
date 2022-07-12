759efdd177ef067692919d838af7d002fc9eafb3
Krishanu Acharya <krishanurocks@gmail.com>
2022-07-12 13:54:53 +0530

    Base Controller Commit

---
+++ src/main/java/com/datacloud/DaCu/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.datacloud.DaCu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BaseController {
+
+    @GetMapping("/user")
+    public String testView()    {
+        return "Welcome Krishanu Acharya";
+    }
+}
