bad1c489ad96a6a723c13a20d7bbc88e710a8fbc
Krishanu Acharya <krishanurocks@gmail.com>
2022-07-12 13:42:27 +0530

    Test Push

---
+++ src/main/java/com/datacloud/DaCu/DaCuApplication.java
@@ -0,0 +1,13 @@
+package com.datacloud.DaCu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaCuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaCuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datacloud/DaCu/DaCuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datacloud.DaCu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaCuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
