0b8bc911e9b92b70318b0bebee8bde51c02b64a2
Krishanu Acharya <krishanurocks@gmail.com>
2022-07-12 16:10:37 +0530

    Rest Service

--- src/main/java/com/datacloud/DaCu/controller/BaseController.java
+++ src/main/java/com/datacloud/DaCu/controller/BaseController.java
@@ -1,21 +1,32 @@
 package com.datacloud.DaCu.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class BaseController {
 
-    @GetMapping("user")
-    public String testView(ModelMap map) {
-        map.addAttribute("message", "Welcome Krishanu Acharya!");
-        return "user";
-    }
-
     @GetMapping("/")
-    public String homeView(ModelMap map) {
-        map.addAttribute("msg", "Test Message!");
-        return "test";
+    public ResponseEntity<List<String>> getBankList(@RequestParam(name = "name", required = false) String name) {
+        try {
+            List<String> stringArrayList = new ArrayList<String>();
+            if(!stringArrayList.contains(name))
+                stringArrayList.add(name);
+            if (stringArrayList.isEmpty())
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(stringArrayList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
